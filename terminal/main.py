9810f7b7f9113ea3b9c9d09fbd1da1abcfedcae6
Viraj <81317525+viraj-mewal@users.noreply.github.com>
2024-02-17 10:50:00 +0530

    Initial Commit

---
+++ terminal/main.py
@@ -0,0 +1,42 @@
+# create a weather app that shows weather of a place
+
+import requests
+import json
+import os
+
+
+api = "78724d4414ec451682843153241702"
+
+os.system("cls")
+print(f"\t\t WELCOME TO THE WEATHER APP \t\t\n\n")
+
+place = input("Enter name of place :- ")
+
+# Fetching the weather details
+print("Fetching......")
+
+place = "%20".join(place.split(" "))
+source = f"https://api.weatherapi.com/v1/current.json?key={api}&q={place}&aqi=no"
+got = requests.get(source).text
+data = json.loads(got)
+
+info = {
+    "name": data["location"]["name"],
+    "country": data["location"]["country"],
+    "time": data["location"]["localtime"],
+    "temp": data["current"]["temp_c"],
+    "weather": data["current"]["condition"]["text"],
+    "wind": data["current"]["wind_kph"],
+    "humidity": data["current"]["humidity"]
+}
+
+print(f'''\n
+Place       :- {info["name"]}
+Country     :- {info["country"]}
+Time        :- {info["time"]}
+Temperature :- {info["temp"]}°C
+Weather     :- {info["weather"]}
+Wind        :- {info["wind"]} kmph
+Humidity    :- {info["humidity"]}''')
+
+
